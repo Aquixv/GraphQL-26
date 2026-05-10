3ced9df03bc598c5c958254eb18f328c5c027586
Aquiii <dareakintundealamu@gmail.com>
2026-05-10 20:53:47 +0100

    changes

--- src/resolver.ts
+++ src/resolver.ts
@@ -1,6 +1,15 @@
+const mockProducts = [
+  { id: '1', name: 'White Faux Leather Backpack', price: 39.99, inStock: true },
+  { id: '2', name: 'Mechanical Keyboard', price: 120.00, inStock: false },
+  { id: '3', name: 'Wireless Mouse', price: 45.50, inStock: true },
+];
+
 export const resolvers = {
   Query: {
     hello: () => 'Hello from your new GraphQL Playground! 🚀',
-    serverStatus: () => 'Alive and kicking on port 4000!',
+    
+    products: () => mockProducts,
+    
+    product: (_:number, args:string) => mockProducts.find(p => p.id === args.id),
   },
 };
--- src/typeDefs.ts
+++ src/typeDefs.ts
@@ -1,6 +1,16 @@
 export const typeDefs = `#graphql
+  # 1. The Blueprint (Notice the exclamation marks mean "Required/Cannot be null")
+  type Product {
+    id: ID!
+    name: String!
+    price: Float!
+    inStock: Boolean!
+  }
+
+  # 2. The Menu
   type Query {
     hello: String
-    serverStatus: String
+    products: [Product!]!          # Returns an array of Products
+    product(id: ID!): Product      # Accepts an 'id' argument and returns one Product
   }
 `;
